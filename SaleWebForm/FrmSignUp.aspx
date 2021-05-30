f66bc9d6676636e269a7fc2bd65a589d96c14aa4
Thin tran <trvathin@gmail.com>
2021-05-30 16:12:34 +0700

    UPDATE

--- SaleWebForm/FrmSignUp.aspx
+++ SaleWebForm/FrmSignUp.aspx
@@ -1,10 +1,12 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FrmLogin.aspx.cs" Inherits="SaleWebForm.FrmLogin" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FrmSignUp.aspx.cs" Inherits="SaleWebForm.FrmSignUp" %>
 
 <!DOCTYPE html>
 
+<!DOCTYPE html>
 <html>
+
 <head runat="server">
-    <title>Đăng nhập</title>
+    <title>Đăng ký</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
 </head>
 
@@ -13,9 +15,15 @@
         <div class="container d-flex justify-content-center">
             <div class="col-md-4">
                 <asp:Label ID="LabelWarning" runat="server"></asp:Label>
-                <h4 class="card-title mb-3">Đăng nhập</h4>
+                <h4 class="card-title mb-3">Đăng ký</h4>
+
                 <form id="formLogin" runat="server">
                     <div>
+                        <div class="form-group">
+                            <label for="TextBoxFullName">Họ và tên</label>
+                            <asp:TextBox ID="TextBoxFullName" runat="server" class="form-control" TextMode="SingleLine"></asp:TextBox>
+                        </div>
+
                         <div class="form-group">
                             <label for="TextBoxUserName">Tên đăng nhập</label>
                             <asp:TextBox ID="TextBoxUserName" runat="server" class="form-control" TextMode="SingleLine"></asp:TextBox>
@@ -26,12 +34,16 @@
                             <asp:TextBox ID="TextBoxPassWord" runat="server" class="form-control" TextMode="Password"></asp:TextBox>
                         </div>
 
-                        <asp:Button ID="BtnLogin" runat="server" Text="Đăng nhập" CssClass="btn btn-primary btn-block mb-4" OnClick="BtnLogin_Click" />
+                        <div class="form-group">
+                            <label for="TextBoxPassWord">Xác nhận mật khẩu</label>
+                            <asp:TextBox ID="TextBoxPassWordRepeat" runat="server" class="form-control" TextMode="Password"></asp:TextBox>
+                        </div>
+
+                        <asp:Button ID="BtnSignup" runat="server" Text="Đăng ký" CssClass="btn btn-primary btn-block mb-4" OnClick="BtnSignup_Click" />
                     </div>
                 </form>
-                <a href="CommingSoon.html" class="forgot-password-link">Quên mật khẩu?</a>
 
-                <p>Bạn không có tài khoản? <a href="FrmSignUp.aspx" class="text-reset">Đăng ký</a></p>
+                <p>Bạn có tài khoản? <a href="FrmLogin.aspx" class="text-reset">Đăng nhập ngay</a></p>
             </div>
         </div>
     </main>
---
+++ SaleWebForm/helpers/UserHelpers.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SaleWebForm
+{
+    public class UserHelpers
+    {
+        /// <summary>
+        /// Đăng nhập
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="passWord"></param>
+        /// <returns>
+        /// Đăng nhập thành công trả về mã người dùng, ngược lại trả về -1
+        /// </returns>
+        public static int Login(string userName, string passWord)
+        {
+            string passWordEndcode = Helpers.EncodePassWord(passWord);
+
+            using (DataClassesDataContext db = new DataClassesDataContext())
+            {
+                IQueryable<tblUser> query = from item in db.tblUsers
+                                            where (item.userName == userName) && (item.passWord == passWordEndcode)
+                                            select item;
+
+                if (query.Count() != 0)
+                {
+                    return query.First().userId;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="ignoreUserId"></param>
+        /// <returns>
+        /// Return true nếu đã được sử dụng
+        /// </returns>
+        public static bool CheckUserNameExist(string userName, int ignoreUserId = -1)
+        {
+            using (DataClassesDataContext db = new DataClassesDataContext())
+            {
+                int cnt = db.tblUsers.Count(user => user.userName == userName && user.userId != ignoreUserId);
+                return cnt > 0;
+            }
+        }
+
+        public static tblUser GetUserById(int userID)
+        {
+            using (DataClassesDataContext db = new DataClassesDataContext())
+            {
+                tblUser query = db.tblUsers.First(user => user.userId == userID);
+
+                return query;
+            }
+        }
+
+        public static void Add(string fullName, string userName, string passWord)
+        {
+            using (DataClassesDataContext db = new DataClassesDataContext())
+            {
+                tblUser itemNewAdd = new tblUser();
+
+                string passWordEndcode = Helpers.EncodePassWord(passWord);
+                itemNewAdd.fullName = fullName;
+                itemNewAdd.userName = userName;
+                itemNewAdd.passWord = passWordEndcode;
+
+                db.tblUsers.InsertOnSubmit(itemNewAdd);
+                db.SubmitChanges();
+            }
+        }
+
+        public static bool UpdateUser(int userID, string fullName, string passWord)
+        {
+            using (DataClassesDataContext db = new DataClassesDataContext())
+            {
+                try
+                {
+                    tblUser objUser = db.tblUsers.Single(user => user.userId == userID);
+
+                    string passWordEndcode = Helpers.EncodePassWord(passWord);
+
+                    objUser.fullName = fullName;
+                    objUser.passWord = passWordEndcode;
+
+                    db.SubmitChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    // Debug
+                    Debug.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+        }
+
+        public static void Edit(int userID, string fullName, string userName, string passWord)
+        {
+            using (DataClassesDataContext db = new DataClassesDataContext())
+            {
+                var itemNeedEdit = db.tblUsers.First(user => user.userId == userID);
+
+                string passWordEndcode = Helpers.EncodePassWord(passWord);
+                itemNeedEdit.fullName = fullName;
+                itemNeedEdit.userName = userName;
+                itemNeedEdit.passWord = passWordEndcode;
+
+                db.SubmitChanges();
+            }
+        }
+
+        public static void Delete(int userID)
+        {
+            using (DataClassesDataContext db = new DataClassesDataContext())
+            {
+                var itemNeedDel = db.tblUsers.First(user => user.userId == userID);
+
+                db.tblUsers.DeleteOnSubmit(itemNeedDel);
+                db.SubmitChanges();
+            }
+        }
+    }
+}
